7e9ede1d5359b31b40706af6dd8c20640f488cc3
Kieran Hillman <developedbykieran@gmail.com>
2023-11-22 12:55:30 +0000

    Create new sign up form view

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from item.models import Category, Item
 
+from .forms import SignUpForm
+
 # Create your views here.
 
 
@@ -17,3 +19,12 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+
+def signup(request):
+    form = SignUpForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'core/signup.html', context)
